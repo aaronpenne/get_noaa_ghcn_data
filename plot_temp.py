415b9e93931031a183a404502f2976197f558e25
Aaron Penne <apenne@amazon.com>
2019-05-14 09:02:00 -0700

    Tweaked plot titles to be more accurate

--- plot_temp.py
+++ plot_temp.py
@@ -18,8 +18,9 @@ if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
 
 # Get input data
-input_file = os.path.join('data', 'usc.csv')
-df = pd.read_csv(input_file, parse_dates=[0])
+input_file_name = 'usc.csv'
+input_file_path = os.path.join('data', input_file_name)
+df = pd.read_csv(input_file_path, parse_dates=[0])
 
 # FIXME or do something with scatter plot of hottest day each week, color if max
 
@@ -40,18 +41,18 @@ for year in range(1922, 2018):
         if segment == year_list[-1]:
             plt.plot(x, y, color='#800000', alpha=alpha)
         else:
-            plt.plot(x, y, color='#3f3f3f', alpha=alpha)        
-        plt.title(year)
+            plt.plot(x, y, color='#3f3f3f', alpha=alpha)
+        plt.title('{} - {}'.format(input_file_name, year))
     plt.ylim(int(math.floor(df['TMAX'].min() / 100.0)) * 100, int(math.ceil(df['TMAX'].max() / 100.0)) * 100)
     plt.xlim(datetime(2000, 1, 1), datetime(2000, 12, 31))
     ax.xaxis.set_major_locator(months)
     ax.xaxis.set_major_formatter(monthsFmt)
-    fig.savefig(os.path.join(output_dir, '{}.png'.format(year)),
+    fig.savefig(os.path.join(output_dir, '{}_{}.png'.format(input_file_name.split('.')[0], year)),
             dpi=fig.dpi,
             bbox_inches='tight',
             pad_inches=0.3)
     plt.close()
-        
+
 # Make gif
 png_files = [f for f in os.listdir(output_dir) if f.endswith('.png')]
 png_files.sort()
@@ -66,4 +67,4 @@ for i, f in enumerate(png_files):
             charts.append(imageio.imread(os.path.join(output_dir, f)))
 
 # Save gif
-imageio.mimsave(os.path.join(output_dir, 'temperature_boston_smooth_30.gif'), charts, format='GIF', duration=0.2)
+imageio.mimsave(os.path.join(output_dir, 'temperature_smooth_{}.gif'.format(input_file_name.split('.')[0])), charts, format='GIF', duration=0.2)
